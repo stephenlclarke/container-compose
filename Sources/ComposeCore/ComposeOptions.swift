56e94ab180ed729b23782fe02f869034784d7831
Steve <stephenlclarke@mac.com>
2026-06-16 02:58:31 +0100

    refactor(options): limit normalizer options

--- Sources/ComposeCore/ComposeOptions.swift
+++ Sources/ComposeCore/ComposeOptions.swift
@@ -16,37 +16,25 @@
 
 import Foundation
 
-/// Global Compose options shared by all subcommands.
+/// Options that influence Compose file normalization.
 public struct ComposeOptions: Equatable {
     public var files: [String]
     public var projectName: String?
     public var profiles: [String]
     public var envFiles: [String]
     public var projectDirectory: String?
-    public var ansi: String?
-    public var progress: String?
-    public var dryRun: Bool
-    public var verbose: Bool
 
     public init(
         files: [String] = [],
         projectName: String? = nil,
         profiles: [String] = [],
         envFiles: [String] = [],
-        projectDirectory: String? = nil,
-        ansi: String? = nil,
-        progress: String? = nil,
-        dryRun: Bool = false,
-        verbose: Bool = false
+        projectDirectory: String? = nil
     ) {
         self.files = files
         self.projectName = projectName
         self.profiles = profiles
         self.envFiles = envFiles
         self.projectDirectory = projectDirectory
-        self.ansi = ansi
-        self.progress = progress
-        self.dryRun = dryRun
-        self.verbose = verbose
     }
 }
