a479a463f21121028e9542c04db12a72fd7b6ec5
Steve <stephenlclarke@mac.com>
2026-07-01 20:35:06 +0100

    fix(homebrew): update source formula to stable release

--- Formula/container-compose.rb
+++ Formula/container-compose.rb
@@ -1,9 +1,9 @@
 class ContainerCompose < Formula
   desc "Docker Compose style plugin for Apple's container CLI"
   homepage "https://github.com/stephenlclarke/container-compose"
-  url "https://github.com/stephenlclarke/container-compose/releases/download/0.6.0/container-compose-plugin-release-arm64.tar.gz"
-  version "0.6.0"
-  sha256 "7f9cee3305173344d60422528f76e0b75e565b54f5fd35d3a4a260e1bbcd6b29"
+  url "https://github.com/stephenlclarke/container-compose/releases/download/0.6.1/container-compose-plugin-release-arm64.tar.gz"
+  version "0.6.1"
+  sha256 "81cc0a099b524b1592d460698d83f2fb2d9b5b4450148dc52bd4747fb40994c8"
   license "Apache-2.0"
 
   depends_on arch: :arm64
@@ -28,13 +28,13 @@ class ContainerCompose < Formula
         brew postinstall stephenlclarke/tap/container
         brew services restart stephenlclarke/tap/container
 
-      This formula installs the stable release prebuilt package asset:
+      This formula installs the Latest prebuilt package asset:
         container-compose-plugin-release-arm64.tar.gz
     EOS
   end
 
   test do
-    assert_match "0.6.0", shell_output("#{bin}/container-compose version --short")
+    assert_match "0.6.1", shell_output("#{bin}/container-compose version --short")
     assert_path_exists libexec/"container-plugins/compose/config.toml"
     assert_predicate libexec/"container-plugins/compose/resources/compose-normalizer", :executable?
   end
