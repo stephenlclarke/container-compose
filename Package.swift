ad56fe8205de2851f82a8191c2ee3edcae6e0b52
Steve <stephenlclarke@mac.com>
2026-07-13 04:44:24 +0100

    chore(deps): pin containerization 03f8a105813a
    Release-Highlight: Release automation pins stephenlclarke/containerization by exact SwiftPM revision 03f8a105813a.

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
         .package(name: "container", path: "../container"),
         .package(
             url: "https://github.com/stephenlclarke/containerization.git",
-            revision: "41252f26870bc875ea0b3e97e1bb656456f02288"
+            revision: "03f8a105813ae680757f4f456a53130c11c0bf66"
         ),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
